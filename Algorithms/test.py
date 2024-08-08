bf640733212ba2ec10574a0d44cbbcc1f6949ef3
CorkiWarehouse <jyan326@aucklanduni.ac.nz>
2024-08-08 19:38:15 +1200

    Change the calculation part of the algorithm. And improve that if our state with more than 1 dimension. And give a new environment which is called flock.
    Next, we need to improve the mean field's residual. We may add one variable which is used to make the improvement more reasonable.

--- Algorithms/test.py
+++ Algorithms/test.py
@@ -1,28 +1,44 @@
-import numpy as np
+import torch
 
-# 定义 velocity_option 和 state_option
-velocity_option = np.round(np.arange(1 / 8, 1 + 1 / 8, 1 / 8), 3)
-state_option = np.round(np.arange(0, 1, 1 / 8), 3)
 
-print(len(velocity_option))
-print(len(state_option))
+def prepare_tensors(state=None, action=None, mf=None, time=None):
+    """
+    Convert inputs to PyTorch tensors with appropriate shapes, and return them in a specific order.
+    Inputs can be None, in which case None is returned in their place.
 
-# 创建一个字典来存储结果
-result_mapping = {}
+    Args:
+        state: The state input which can be a list, scalar, or tensor. Default is None.
+        action: The action input which can be a list, scalar, or tensor. Default is None.
+        mf: The mean-field input which can be a list, scalar, or tensor. Default is None.
+        time: The time input which can be a list, scalar, or tensor. Default is None.
 
-# 遍历所有 velocity 和 state 的组合
-for state in state_option:
-    for velocity in velocity_option:
-        # 计算 velocity 和 state 的和
-        combined_value = np.round(velocity*1/8 + state, 3)
+    Returns:
+        tuple: A tuple of tensors or None, corresponding to state, action, mf, and time in that order.
+    """
+    inputs = {'state': state, 'action': action, 'mf': mf, 'time': time}
+    results = []
+    for key in ['state', 'action', 'mf', 'time']:
+        input_data = inputs[key]
+        if input_data is not None:
+            if not isinstance(input_data, torch.Tensor):
+                input_data = torch.tensor(input_data, dtype=torch.float32)
+            if input_data.dim() == 0:
+                input_data = input_data.unsqueeze(0)  # Convert scalar to 1D tensor
+            tensor = input_data
+            results.append(tensor)
+        else:
+            results.append(None)
 
-        # 找到 state_option 中最接近的值
-        closest_value = min(state_option, key=lambda x: abs(x - combined_value))
+    return tuple(results)
 
-        # 将结果存储到字典中
-        if combined_value not in result_mapping:
-            result_mapping[combined_value] = closest_value
+# Example usage with state and mf provided, expecting to unpack state and mf directly
+state_input = [1, 2, 3]
+mf_input = [0.1, 0.2, 0.3]
+state_tensor, action_tensor, mf_tensor, time_tensor = prepare_tensors(state=state_input, mf=mf_input)
 
-# 打印结果
-for combined_value, closest_value in result_mapping.items():
-    print(f"Combined Value: {combined_value} -> Closest State Option: {closest_value}")
+# Print to see the result
+print('State tensor:', state_tensor)
+print('Action tensor:', action_tensor)  # This should print None
+print('MF tensor:', mf_tensor)
+print('Time tensor:', time_tensor)  # This should print None
+
